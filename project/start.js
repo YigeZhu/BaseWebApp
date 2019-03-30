83a82648a6cfbfa32ffbb2f60b3f96ff55f60bbb
YigeZhu <33578437+YigeZhu@users.noreply.github.com>
2019-03-30 13:43:18 -0400

    Update start.js

--- project/start.js
+++ project/start.js
@@ -9,6 +9,19 @@ app.use(express.static(__dirname));
 app.set('views', __dirname + '/html');
 app.set('view engine', 'ejs');
 
+// Set up the app's routing
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.get('/about', function(request, response) {
+  response.render('pages/about');
+});
+
+app.get('/projects', function(request, response) {
+  response.render('pages/projects');
+});
+
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
